52442b2a11744e5e254dc2f9ed87dc313fe842a7
Federico R. Figueredo <federico.r.figueredo@protonmail.com>
2023-08-13 19:57:59 -0300

    Initial commit

---
+++ build.sh
@@ -0,0 +1,4 @@
+export glibc_install="../glibc/build/install"
+set -eux
+gcc -L "${glibc_install}/lib" -I "${glibc_install}/include" -Wl,--rpath="${glibc_install}/lib" -Wl,--dynamic-linker="${glibc_install}/lib/ld-linux-x86-64.so.2" -std=c11 -o test_glibc.out -v test_glibc.c -pthread
+ldd ./test_glibc.out
