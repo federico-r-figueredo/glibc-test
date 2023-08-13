43dbbafd1677c77fa322339753ddff5e1f9bb67f
Federico R. Figueredo <federico.r.figueredo@protonmail.com>
2023-08-13 20:28:54 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-export glibc_install="../glibc/build/install"
+export glibc_install="/media/fedex/Tertiary/Projects/C/glibc/build/install"
 set -eux
-gcc -L "${glibc_install}/lib" -I "${glibc_install}/include" -Wl,--rpath="${glibc_install}/lib" -Wl,--dynamic-linker="${glibc_install}/lib/ld-linux-x86-64.so.2" -std=c11 -o /build/test_glibc.out -v test_glibc.c -pthread
-ldd ./bulid/test_glibc.out
+gcc -L "${glibc_install}/lib" -I "${glibc_install}/include" -Wl,--rpath="${glibc_install}/lib" -Wl,--dynamic-linker="${glibc_install}/lib/ld-linux-x86-64.so.2" -std=c11 -o ./build/test_glibc.out -v test_glibc.c -pthread
+ldd ./build/test_glibc.out
